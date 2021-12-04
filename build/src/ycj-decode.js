ef40d7e0a120b1711e7b7510c166dec50871c932
liangqin.fan <liangqin.fan@gmail.com>
2021-12-04 23:36:40 +0800

    improve decode method, try to parse function signature

--- build/src/ycj-decode.js
+++ build/src/ycj-decode.js
@@ -1,34 +1,58 @@
 #!/usr/bin/env node
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
 const web3_1 = __importDefault(require("web3"));
+const cross_fetch_1 = __importDefault(require("cross-fetch"));
 commander_1.program
     .requiredOption('--sig <sig...>', 'to use latest version')
     .requiredOption('--data <data>', 'input data to decode');
 commander_1.program.parse(process.argv);
 const options = commander_1.program.opts();
 const web3 = new web3_1.default("https://127.0.0.1");
+function try_parse_signature(hexSig) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const resp = yield (0, cross_fetch_1.default)(`https://www.4byte.directory/api/v1/signatures/?hex_signature=${hexSig}`);
+        if (resp.status !== 200)
+            return hexSig;
+        const dict = yield resp.json();
+        if (dict.count === 0)
+            return hexSig;
+        return dict.results[0].text_signature;
+    });
+}
 function print(funcSig, params) {
     let output = [];
     const len = parseInt(params['__length__']) || 0;
     for (let i = 0; i < len; ++i) {
         output.push(` ${params[i.toString()]}`);
     }
-    console.log(`${funcSig}  ${output}`);
+    console.log(`\n${funcSig}  ${output}`);
 }
 function generate_abi_object(types) {
     return types;
 }
 function main(sig, data) {
-    const abi = generate_abi_object(sig);
-    const funcSig = data.substr(0, 10);
-    const pureData = `0x${data.substr(10)}`;
-    const params = web3.eth.abi.decodeParameters(abi, pureData);
-    print(funcSig, params);
+    return __awaiter(this, void 0, void 0, function* () {
+        const abi = generate_abi_object(sig);
+        let funcSig = data.substr(0, 10);
+        const pureData = `0x${data.substr(10)}`;
+        const params = web3.eth.abi.decodeParameters(abi, pureData);
+        funcSig = yield try_parse_signature(funcSig);
+        print(funcSig, params);
+    });
 }
-main(options.sig, options.data);
+main(options.sig, options.data).catch(e => console.log(e));
 //# sourceMappingURL=ycj-decode.js.map
--- src/ycj-decode.ts
+++ src/ycj-decode.ts
@@ -2,6 +2,7 @@
 
 import {program} from "commander";
 import Web3 from "web3";
+import fetch from 'cross-fetch';
 
 program
   .requiredOption('--sig <sig...>', 'to use latest version')
@@ -12,26 +13,53 @@ program.parse(process.argv);
 const options = program.opts();
 const web3 = new Web3("https://127.0.0.1");
 
+// 尝试解码方法签名
+// curl -X GET https://www.4byte.directory/api/v1/signatures/?hex_signature=0x372f657c
+// {"count":1,"next":null,"previous":null,"results":[{"id":445487,"created_at":"2021-12-04T14:25:07.141219Z","text_signature":"whitelistMint(bytes32[])","hex_signature":"0x372f657c","bytes_signature":"7/e|"}]}
+
+interface DictJson {
+  count: number;
+  next: any;
+  previous: any;
+  results: {
+    id: number;
+    created_at: string;
+    text_signature: string;
+    hex_signature: string;
+    bytes_signature: string;
+  }[]
+}
+async function try_parse_signature(hexSig: string) {
+  const resp = await fetch(`https://www.4byte.directory/api/v1/signatures/?hex_signature=${hexSig}`);
+  if (resp.status !== 200) return hexSig;
+
+  const dict = await resp.json() as DictJson;
+  if (dict.count === 0) return hexSig;
+
+  return dict.results[0].text_signature;
+}
+
 function print(funcSig: string, params: {[key: string]: string}) {
   let output: string[] = [];
   const len = parseInt(params['__length__']) || 0;
   for (let i = 0; i < len; ++i) {
     output.push(` ${params[i.toString()]}`);
   }
-  console.log(`${funcSig}  ${output}`);
+  console.log(`\n${funcSig}  ${output}`);
 }
 
 function generate_abi_object(types: string[]): any[] {
   return types;
 }
 
-function main(sig: string[], data: string) {
+async function main(sig: string[], data: string) {
   const abi = generate_abi_object(sig);
-  const funcSig = data.substr(0, 10);
+  let funcSig = data.substr(0, 10);
   const pureData = `0x${data.substr(10)}`;
 
   const params = web3.eth.abi.decodeParameters(abi, pureData);
+  funcSig = await try_parse_signature(funcSig);
   print(funcSig, params);
 }
 
-main(options.sig, options.data);
+main(options.sig, options.data).catch(e => console.log(e));
