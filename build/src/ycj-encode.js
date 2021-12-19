e51ad356be89e011ee186278fb7c61f0f00678c8
liangqin.fan <liangqin.fan@gmail.com>
2021-12-19 16:44:37 +0800

    enhance encode command

--- build/src/ycj-encode.js
+++ build/src/ycj-encode.js
@@ -7,15 +7,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
 const web3_1 = __importDefault(require("web3"));
 commander_1.program
-    .requiredOption('--sig <sigs...>', 'type to encode')
-    .requiredOption('--data <data...>', 'data to encode');
+    .option('--sig <sigs...>', 'type to encode')
+    .option('--data <data...>', 'data to encode')
+    .option('-f, --func', "encode function signature")
+    .option('-e, --event', "encode event signature");
 commander_1.program.parse(process.argv);
 const options = commander_1.program.opts();
 const web3 = new web3_1.default("https://127.0.0.1");
-function main(sigs, data) {
-    const params = web3.eth.abi.encodeParameters(sigs, data);
-    // console.log(`${sigs} ${data} => ${params.startsWith('0x') ? params.length - 2 : params.length}`);
-    console.log(`\t${params}`);
+function main() {
+    if (options.func) {
+        const fsig = web3.eth.abi.encodeFunctionSignature(options.data[0]);
+        console.log(fsig);
+    }
+    else if (options.event) {
+        const esig = web3.eth.abi.encodeEventSignature(options.data[0]);
+        console.log(esig);
+    }
+    else {
+        const params = web3.eth.abi.encodeParameters(options.sigs, options.data);
+        // console.log(`${sigs} ${data} => ${params.startsWith('0x') ? params.length - 2 : params.length}`);
+        console.log(`\t${params}`);
+    }
 }
-main(options.sigs, options.data);
+main();
 //# sourceMappingURL=ycj-encode.js.map
--- src/ycj-encode.ts
+++ src/ycj-encode.ts
@@ -4,19 +4,30 @@ import {program} from "commander";
 import Web3 from "web3";
 
 program
-  .requiredOption('--sig <sigs...>', 'type to encode')
-  .requiredOption('--data <data...>', 'data to encode');
+  .option('--sig <sigs...>', 'type to encode')
+  .option('--data <data...>', 'data to encode')
+  .option('-f, --func', "encode function signature")
+  .option('-e, --event', "encode event signature");
+
 
 program.parse(process.argv);
 
 const options = program.opts();
 const web3 = new Web3("https://127.0.0.1");
 
-function main(sigs: string[], data: string[]) {
+function main() {
 
-  const params = web3.eth.abi.encodeParameters(sigs, data);
-  // console.log(`${sigs} ${data} => ${params.startsWith('0x') ? params.length - 2 : params.length}`);
-  console.log(`\t${params}`);
+  if (options.func) {
+    const fsig = web3.eth.abi.encodeFunctionSignature(options.data[0]);
+    console.log(fsig);
+  } else if (options.event) {
+    const esig = web3.eth.abi.encodeEventSignature(options.data[0]);
+    console.log(esig);
+  } else {
+    const params = web3.eth.abi.encodeParameters(options.sigs, options.data);
+    // console.log(`${sigs} ${data} => ${params.startsWith('0x') ? params.length - 2 : params.length}`);
+    console.log(`\t${params}`);
+  }
 }
 
-main(options.sigs, options.data);
+main();
